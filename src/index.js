6bb0aabb3024f82675e77f1642b594d61518c1e2
gitTest <heswebs@gmail.com>
2018-11-05 11:00:58 -0800

    added redux middleware, install axios, created action

--- src/index.js
+++ src/index.js
@@ -4,8 +4,19 @@ import "./index.css";
 import App from "./components/app";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import ReduxPromise from "redux-promise";
+import reducers from ".reducers";
 
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
